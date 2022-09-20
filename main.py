91031ca76fc72915c5926605d0ef2fedc5063980
유건욱 <gunwook0307@com2us.com>
2022-09-20 13:25:06 +0900

    [add] timm library

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+import timm # torch image models (like huggingface library)
 import torch
 import random
 import numpy as np
+import pandas as pd
 from transformers import AutoModel, AutoTokenizer
+from sklearn.model_selection import train_test_split
 
 from modules import custom_dataset, trainer
 from models import cv_model, nlp_model, multi_modal_classifier
@@ -20,4 +23,7 @@ def seed_everything(seed):
 
 def main():
     seed_everything(3307)
+    train = pd.read_csv("./data/train.csv")
+    test = pd.read_csv("./data/test.csv")
+
     return
