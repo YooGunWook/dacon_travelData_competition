4ab1dca611944319cd6d8741a92e86bda5daa9fa
Jack <gunwook0307@yonsei.ac.kr>
2022-09-22 15:24:53 +0900

    Update nlp_model.py

--- models/nlp_model.py
+++ models/nlp_model.py
@@ -2,6 +2,55 @@ import torch
 from torch import nn
 
 
+#########################          TOWER          #########################
+class TextCNN(nn.Module):
+    def __init__(self, embedding_dim, kernel_list, num_filters, num_classes, drop_rate):
+        """Text Classification with CNN
+
+        Args:
+            embedding_dim (int): embedding dimension's size
+            kernel_list (list): list of kernel size
+            num_filters (int): filter size (CNN output size)
+            num_classes (int): number of classes
+            drop_rate (float): dropout rate
+        """
+
+        super(TextCNN, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.kernel_list = kernel_list
+        self.num_filters = num_filters
+        self.num_classes = num_classes
+        self.conv = nn.ModuleList(
+            [
+                nn.Conv1d(
+                    in_channels=embedding_dim,
+                    out_channels=num_filters,
+                    kernel_size=k,
+                    stride=1,
+                )
+                for k in kernel_list
+            ]
+        )
+        self.batch_norms = nn.ModuleList(
+            [
+                nn.BatchNorm1d(num_filters)
+                for _ in kernel_list
+            ]
+        )
+        self.dropout = nn.Dropout(drop_rate)
+        self.fc = nn.Linear(len(kernel_list) * num_filters, num_classes)
+
+    def forward(self, x):
+        x = x.transpose(1, 2)
+        x = [F.relu(self.batch_norms[idx](conv(x))) for idx, conv in enumerate(self.conv)]
+        x = [self.dropout(vec) for vec in x]
+        x = [F.max_pool1d(c, c.size(-1)).squeeze(dim=-1) for c in x]
+        feature = torch.cat(x, dim=1)
+        x = self.fc(feature)
+        return x, feature
+
+
+
 class NLPModel(nn.Module):
     def __init__(self, model, config):
         super(NLPModel, self).__init__()
