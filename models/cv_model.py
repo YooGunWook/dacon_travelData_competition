1302e33e2ee5197d0696b6cfed69a8fd84276057
Jack <gunwook0307@yonsei.ac.kr>
2022-09-25 23:55:40 +0900

    Update cv_model.py

--- models/cv_model.py
+++ models/cv_model.py
@@ -7,12 +7,19 @@ class CVModel(nn.Module):
         super(CVModel, self).__init__()
         self.model = model
         self.config = config
-        self.avgpool = nn.AvgPool2d(self.config["kernel_size"])
-        self.dropout = nn.Dropout2d(self.config["dropout"])
+        if not self.config["cv_transformer"]:
+            self.avgpool = nn.AvgPool2d(self.config["kernel_size"])
+            self.dropout = nn.Dropout2d(self.config["dropout"])
+        else:
+            self.avgpool = nn.AvgPool1d(self.config["kernel_size"])
+            self.dropout = nn.Dropout(self.config["dropout"])
         self.linear = nn.Linear(self.config["cv_dim"], 128)
 
     def forward(self, inputs):
         outputs = self.model.forward_features(inputs)
-        outputs = torch.flatten(self.dropout(self.avgpool(outputs)), start_dim=1)
+        if not self.config["cv_transformer"]:
+            outputs = torch.flatten(self.dropout(self.avgpool(outputs)), start_dim=1)
+        else:
+            outputs = self.dropout(self.avgpool(outputs[:, 0, :]))
         outputs = self.linear(outputs)
         return outputs
